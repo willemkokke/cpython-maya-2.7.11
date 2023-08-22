c27b7f1390d2bd85cb459a6eee4f6401ba653bc7
Willem Kokke <willem@humain-studios.com>
2023-08-22 02:00:36 +0100

    Applied https://github.com/kovidgoyal/cpython/commit/c7f7a886e0907af640ab81af25c00f53adf8eb67

--- Lib/platform.py
+++ Lib/platform.py
@@ -501,7 +501,7 @@ def _syscmd_ver(system='', release='', version='',
     # Try some common cmd strings
     for cmd in ('ver','command /c ver','cmd /c ver'):
         try:
-            pipe = popen(cmd)
+            pipe = os.popen(cmd)
             info = pipe.read()
             if pipe.close():
                 raise os.error,'command failed'
